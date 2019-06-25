e90595d5d42bd2c3e0705e4061f160a8fda7587e
Camden Wagner <camden.i.wagner@ivarcode.net>
2019-06-25 16:37:02 -0400

    line endings

--- src/test/keyboard-shortcut.directive.spec.ts
+++ src/test/keyboard-shortcut.directive.spec.ts
@@ -1,85 +1,85 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { async, TestBed, ComponentFixture } from '@angular/core/testing';
-import {} from 'jasmine';
+import { } from 'jasmine';
 
 import { KeyboardShortcutsService } from '../keyboard-shortcut.service';
 import { KeyboardShortcutDirective } from '../keyboard-shortcut.directive';
 import { Component } from '@angular/core';
-import {
-  IKeyboardShortcutListenerOptions,
-  KeyboardKeys
-} from '../libraries/listener.library';
+import { IKeyboardShortcutListenerOptions, KeyboardKeys } from '../libraries/listener.library';
 
 @Component({
-  template: `
-    <form>
-      <input
-        *ngIf="haveElement"
-        (click)="onClick()"
-        [keyboardShortcut]="keyboardShortcut"
-      />
-    </form>
-  `
+    template: `<a *ngIf="haveElement" (click)="onClick()" [keyboardShortcut]="keyboardShortcut"></a>`,
 })
 class TestKeyboardShortcutDirectiveComponent {
-  keyboardShortcut: IKeyboardShortcutListenerOptions = {
-    description: 'close',
-    keyBinding: [KeyboardKeys.Alt, 'C']
-  };
 
-  haveElement = true;
+    keyboardShortcut: IKeyboardShortcutListenerOptions = {
+        description: 'close',
+        keyBinding: [KeyboardKeys.Alt, 'C'],
+    };
 
-  onClick(): void {}
+    haveElement = true;
+
+    onClick(): void {
+    }
+
 }
 
 describe('KeyboardShortcutDirective', () => {
-  let service: KeyboardShortcutsService;
-  let fixture: ComponentFixture<TestKeyboardShortcutDirectiveComponent>;
-  let component: TestKeyboardShortcutDirectiveComponent;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [
-        TestKeyboardShortcutDirectiveComponent,
-        KeyboardShortcutDirective
-      ],
-      imports: [BrowserModule],
-      providers: [KeyboardShortcutsService]
-    });
+    let service: KeyboardShortcutsService;
+    let fixture: ComponentFixture<TestKeyboardShortcutDirectiveComponent>;
+    let component: TestKeyboardShortcutDirectiveComponent;
 
-    service = TestBed.get(KeyboardShortcutsService);
-    fixture = TestBed.createComponent(TestKeyboardShortcutDirectiveComponent);
-    component = fixture.componentInstance;
-  }));
-
-  // ================================================
-
-  describe(':: testing shortcut', () => {
     beforeEach(async(() => {
-      fixture.detectChanges();
+        TestBed.configureTestingModule({
+            declarations: [
+                TestKeyboardShortcutDirectiveComponent,
+                KeyboardShortcutDirective,
+            ],
+            imports: [
+                BrowserModule,
+            ],
+            providers: [
+                KeyboardShortcutsService,
+            ],
+        });
+
+        service = TestBed.get(KeyboardShortcutsService);
+        fixture = TestBed.createComponent(TestKeyboardShortcutDirectiveComponent);
+        component = fixture.componentInstance;
     }));
 
-    it('should click when combination matches', () => {
-      spyOn(component, 'onClick');
-      const event = <KeyboardEvent>{ altKey: true, key: 'C' };
-      service.sendKeyboardEventToHandler(event);
-      expect(component.onClick).toHaveBeenCalled();
+    // ================================================
+
+    describe(':: testing shortcut', () => {
+
+        beforeEach(async(() => {
+            fixture.detectChanges();
+        }));
+
+        it('should click when combination matches', () => {
+            spyOn(component, 'onClick');
+            const event = <KeyboardEvent>{ altKey: true, key: 'C' };
+            service.sendKeyboardEventToHandler(event);
+            expect(component.onClick).toHaveBeenCalled();
+        });
+
+        it('should not click when combination does not match', () => {
+            spyOn(component, 'onClick');
+            const event = <KeyboardEvent>{ altKey: true, key: 'Z' };
+            service.sendKeyboardEventToHandler(event);
+            expect(component.onClick).not.toHaveBeenCalled();
+        });
+
+        it('should remove listener when element is removed', () => {
+            const startingCount = service.listeners_read_only.length;
+            expect(startingCount).toBe(1);
+            component.haveElement = false;
+            fixture.detectChanges();
+            const endingCount = service.listeners_read_only.length;
+            expect(endingCount).toBe(0);
+        });
+
     });
 
-    it('should not click when combination does not match', () => {
-      spyOn(component, 'onClick');
-      const event = <KeyboardEvent>{ altKey: true, key: 'Z' };
-      service.sendKeyboardEventToHandler(event);
-      expect(component.onClick).not.toHaveBeenCalled();
-    });
-
-    it('should remove listener when element is removed', () => {
-      const startingCount = service.listeners_read_only.length;
-      expect(startingCount).toBe(1);
-      component.haveElement = false;
-      fixture.detectChanges();
-      const endingCount = service.listeners_read_only.length;
-      expect(endingCount).toBe(0);
-    });
-  });
 });
