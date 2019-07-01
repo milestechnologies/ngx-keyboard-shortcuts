a8476680fc28a0672b3d5fac26b14f931c581a64
Camden Wagner <camden.i.wagner@ivarcode.net>
2019-07-01 15:01:18 -0400

    all the prettier/linter changes ever

--- src/demo/app/demo.component.ts
+++ src/demo/app/demo.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
     template: `
         <h2>Demo Component</h2>
-    `,
+    `
 })
 export class DemoComponent {
     constructor() {}
--- src/keyboard-shortcut-help-window.service.ts
+++ src/keyboard-shortcut-help-window.service.ts
@@ -1,8 +1,8 @@
 import {
-  Injectable,
-  ComponentRef,
-  ViewContainerRef,
-  ComponentFactoryResolver,
+    Injectable,
+    ComponentRef,
+    ViewContainerRef,
+    ComponentFactoryResolver
 } from '@angular/core';
 
 import { SwalComponent } from '@sweetalert2/ngx-sweetalert2';
@@ -10,98 +10,98 @@ import { SweetAlertOptions } from 'sweetalert2';
 
 import { KeyboardShortcutsService } from './keyboard-shortcut.service';
 import {
-  KeyboardShortcutCombination,
-  KeyboardKeys,
+    KeyboardShortcutCombination,
+    KeyboardKeys
 } from './libraries/listener.library';
 
 @Injectable()
 export class KeyboardShortcutHelpWindowService {
-  private swalRef: ComponentRef<SwalComponent>;
-  public swalInstance: SwalComponent;
+    private swalRef: ComponentRef<SwalComponent>;
+    public swalInstance: SwalComponent;
 
-  options: SweetAlertOptions = {
-    html: '',
-    showCancelButton: false,
-    showCloseButton: false,
-    showConfirmButton: false,
-    title: 'Keyboard Shortcuts',
-  };
+    options: SweetAlertOptions = {
+        html: '',
+        showCancelButton: false,
+        showCloseButton: false,
+        showConfirmButton: false,
+        title: 'Keyboard Shortcuts'
+    };
 
-  get helpWindowAvailable(): boolean {
-    return this.swalInstance ? true : false;
-  }
-
-  public constructor(
-    private readonly resolver: ComponentFactoryResolver,
-    private readonly keyboardShortcutsService: KeyboardShortcutsService,
-  ) {}
-
-  public setViewContainerRef(
-    viewContainerRef: ViewContainerRef,
-    helpKeyBinding?: KeyboardShortcutCombination,
-  ): void {
-    // bypass if already initialized
-    if (this.swalInstance) {
-      return;
+    get helpWindowAvailable(): boolean {
+        return this.swalInstance ? true : false;
     }
 
-    // create the swal
-    this.createSwal(viewContainerRef);
+    public constructor(
+        private readonly resolver: ComponentFactoryResolver,
+        private readonly keyboardShortcutsService: KeyboardShortcutsService
+    ) {}
 
-    // add binding to show the help window
-    const defaultBinding: KeyboardShortcutCombination = [
-      KeyboardKeys.Alt,
-      KeyboardKeys.Shift,
-      'h',
-    ];
-    const keyBinding = helpKeyBinding || defaultBinding;
-    this.keyboardShortcutsService.listen({
-      description: null,
-      handler: this.showHelpWindow.bind(this),
-      keyBinding: keyBinding,
-      omitFromHelp: true,
-    });
-  }
+    public setViewContainerRef(
+        viewContainerRef: ViewContainerRef,
+        helpKeyBinding?: KeyboardShortcutCombination
+    ): void {
+        // bypass if already initialized
+        if (this.swalInstance) {
+            return;
+        }
 
-  public showHelpWindow(): void {
-    if (this.swalInstance) {
-      this.options.html = this.buildHtml();
-      this.swalInstance.options = this.options;
-      this.swalInstance.show();
-    } else {
-      console.warn(
-        'KeyboardShortcutHelpWindowService: Failed to show help window since swalInstance was not found.  Ensure setViewContainerRef has been called',
-      );
+        // create the swal
+        this.createSwal(viewContainerRef);
+
+        // add binding to show the help window
+        const defaultBinding: KeyboardShortcutCombination = [
+            KeyboardKeys.Alt,
+            KeyboardKeys.Shift,
+            'h'
+        ];
+        const keyBinding = helpKeyBinding || defaultBinding;
+        this.keyboardShortcutsService.listen({
+            description: null,
+            handler: this.showHelpWindow.bind(this),
+            keyBinding: keyBinding,
+            omitFromHelp: true
+        });
     }
-  }
 
-  private createSwal(viewContainerRef: ViewContainerRef): void {
-    const factory = this.resolver.resolveComponentFactory(SwalComponent);
-    this.swalRef = viewContainerRef.createComponent(factory);
-    this.swalInstance = this.swalRef.instance;
-  }
+    public showHelpWindow(): void {
+        if (this.swalInstance) {
+            this.options.html = this.buildHtml();
+            this.swalInstance.options = this.options;
+            this.swalInstance.show();
+        } else {
+            console.warn(
+                'KeyboardShortcutHelpWindowService: Failed to show help window since swalInstance was not found.  Ensure setViewContainerRef has been called'
+            );
+        }
+    }
 
-  private buildHtml(): string {
-    let html = `
+    private createSwal(viewContainerRef: ViewContainerRef): void {
+        const factory = this.resolver.resolveComponentFactory(SwalComponent);
+        this.swalRef = viewContainerRef.createComponent(factory);
+        this.swalInstance = this.swalRef.instance;
+    }
+
+    private buildHtml(): string {
+        let html = `
         <br/>
         <div style="text-align: left; margin-left: 30px;">
         `;
-    const shortcutsToShow = this.keyboardShortcutsService.listeners_read_only.filter(
-      (ks) => !ks.omitFromHelp,
-    );
-    shortcutsToShow.forEach((s) => {
-      html += `
+        const shortcutsToShow = this.keyboardShortcutsService.listeners_read_only.filter(
+            (ks) => !ks.omitFromHelp
+        );
+        shortcutsToShow.forEach((s) => {
+            html += `
             <div style="margin-bottom: 25px"><strong class="well well-sm">${
-              s.displayShortcutCombination
+                s.displayShortcutCombination
             }</strong><span style="margin-left: 15px">${
-        s.description
-      }</span></div>
+                s.description
+            }</span></div>
             `;
-    });
-    html += `
+        });
+        html += `
         </div>
         <br/>
         `;
-    return html;
-  }
+        return html;
+    }
 }
