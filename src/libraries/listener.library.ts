e18ecf946904e9837452ae508bb9b64635e42459
Mark Valeriani <markv2311@gmail.com>
2019-07-09 15:36:42 -0400

    did all the documentation for this library

--- src/libraries/listener.library.ts
+++ src/libraries/listener.library.ts
@@ -1,11 +1,29 @@
 import { BlackListedKeyboardShortcutChecker } from './black-listed-key-bindings.library';
 
-export interface IKeyboardShortcutListenerOptions {
+export interface IKeyboardShortcutListenerOptions { // defaults with ?
+    /**
+     * @property {KeyboardShortcutCombination}
+     */
     keyBinding: KeyboardShortcutCombination;
+    /**
+     * @property {string} to explain to the user what the shortcut combination you made will do
+     */
     description: string;
+    /**
+     * @property { number } the order of presedence when more than one key matches
+     */
     priority?: number;
+    /**
+     * @property { boolean } if this is true, and you are inside a text box and do shortcut it wouldn't actually fire
+     */
     passToLowerPriorities?: boolean;
+    /**
+     * @property { boolean } if this is true, and you are inside a text box and do shortcut it wouldn't actually fire
+     */
     ignoreEventsFromInputElement?: boolean;
+    /**
+     * @property { boolean } if this is true it doesn't show in the help window
+     */
     omitFromHelp?: boolean;
 }
 
@@ -15,26 +33,80 @@ export interface IKeyboardShortcutListenerConstructorObject
 }
 
 export interface IKeyboardShortcutListener {
+    /**
+     * @property {KeyboardShortcutCombination}
+     */
     keyBinding: KeyboardShortcutCombination;
+    /**
+     * @property {string} to explain to the user what the shortcut combination you made will do
+     */
     description: string;
+    /**
+     * @property {KeyboardShortcutHandler} 
+     */
     handler: KeyboardShortcutHandler;
+    /**
+     * @property { number } the order of presedence when more than one key matches
+     */
     priority: number;
+    /**
+     * @property {boolean} if this is true then pass down from prioritze low to high, if false do the opposite
+     */
     passToLowerPriorities: boolean;
+    /**
+     * @property { boolean } if this is true, and you are inside a text box and do shortcut it wouldn't actually fire
+     */
     ignoreEventsFromInputElement: boolean;
+    /**
+     * @property { string } representation of the keyboard combination normalized for processing
+     */
     mappedKeyboardShortcutCombination: string;
+    /**
+     * @property { string } displays a printer friendly version of the key combination
+     */
     displayShortcutCombination: string;
+    /**
+     * @property { boolean } if this is true it doesn't show in the help window
+     */
     omitFromHelp: boolean;
 }
 
 export class KeyboardShortcutListener implements IKeyboardShortcutListener {
+    /**
+     * @property {KeyboardShortcutCombination}
+     */
     keyBinding: KeyboardShortcutCombination;
+    /**
+     * @property {string} to explain to the user what the shortcut combination you made will do
+     */
     description: string;
+    /**
+     * @property { number } the order of presedence when more than one key matches
+     */
     priority: number;
+    /**
+     * @property {KeyboardShortcutHandler} 
+     */
     handler: KeyboardShortcutHandler;
+    /**
+     * @property {boolean} if this is true then pass down from prioritze low to high, if false do the opposite
+     */
     passToLowerPriorities: boolean;
+    /**
+     * @property { boolean } if this is true, and you are inside a text box and do shortcut it wouldn't actually fire
+     */
     ignoreEventsFromInputElement: boolean;
+    /**
+     * @property { string } representation of the keyboard combination normalized for processing
+     */
     mappedKeyboardShortcutCombination: string;
+    /**
+     * @property { string } displays a printer friendly version of the key combination
+     */
     displayShortcutCombination: string;
+    /**
+     * @property { boolean } if this is true it doesn't show in the help window
+     */
     omitFromHelp: boolean;
 
     constructor(
@@ -80,22 +152,36 @@ export class KeyboardShortcutListener implements IKeyboardShortcutListener {
         }
     }
 }
-
+/**
+ * @interface represents the handle of a listener
+ */
 export interface IListenerHandle {
+    /**
+     * @description removes this listener
+     */
     remove(): void;
 }
-
+/**
+ * @type { event: KeyboardEvent } it is handler to keep track of the listeners
+ */
 export type KeyboardShortcutHandler = (event: KeyboardEvent) => void;
-
+/**
+ * @enum this allows you to map special keys into your Keyboard Combination
+ */
 export enum KeyboardKeys {
     Ctrl = 'ctrl',
     Alt = 'alt',
     Shift = 'shift',
     Escape = 'escape'
 }
-
-export type KeyboardShortcutCombination = (string | KeyboardKeys)[];
-
+/**
+ * @type the combination of keys that will call the shortcut you use
+ */
+export type KeyboardShortcutCombination = (string | KeyboardKeys)[]; // not sure yet chris take care of this 
+/**
+ * @param bindings takes in the shortcut from KeyboardShortcutCombination which is an array of strings
+ * @return {string} // look up
+ */
 export function mapKeyboardShortcutCombination(
     bindings: KeyboardShortcutCombination
 ): string {
@@ -105,6 +191,11 @@ export function mapKeyboardShortcutCombination(
 // Map to normalized keys across different browser implementations.
 // https://github.com/angular/angular/blob/5.0.5/packages/platform-browser/src/browser/browser_adapter.ts#L25-L42
 // tslint:disable:object-literal-sort-keys
+/**
+ * @constant
+ * Map to convert some key or keyIdentifier values to what will be returned by getEventKey
+ * This is also used to help browser compatibility and to match the W3C standard
+ */
 export const KEY_MAP = {
     '\b': 'backspace',
     '\t': 'tab',
