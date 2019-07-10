8f35272cbd7d5891eb07c2c221c087bc9e2731e4
Anthony Massari <amassariiv@gmail.com>
2019-07-10 08:58:23 -0400

    Removed all window service and SweetAlert2

--- src/index.ts
+++ src/index.ts
@@ -3,21 +3,17 @@ import { CommonModule } from '@angular/common';
 
 import { KeyboardShortcutService } from './keyboard-shortcut.service';
 import { KeyboardShortcutDirective } from './keyboard-shortcut.directive';
-import { KeyboardShortcutHelpWindowService } from './keyboard-shortcut-help-window.service';
-import { SweetAlert2Module } from '@sweetalert2/ngx-sweetalert2';
 
 @NgModule({
     declarations: [KeyboardShortcutDirective],
-    exports: [KeyboardShortcutDirective],
-    imports: [CommonModule, SweetAlert2Module.forRoot()]
+    exports: [KeyboardShortcutDirective]
 })
 export class NgxKeyboardShortcutModule {
     static forRoot(): any {
         return {
             ngModule: NgxKeyboardShortcutModule,
             providers: [
-                KeyboardShortcutService,
-                KeyboardShortcutHelpWindowService
+                KeyboardShortcutService
             ]
         };
     }
--- src/public_api.ts
+++ src/public_api.ts
@@ -1,5 +1,4 @@
 export * from './keyboard-shortcut.directive';
 export * from './keyboard-shortcut.service';
-export * from './keyboard-shortcut-help-window.service';
 export * from './libraries/listener.library';
 export * from './index';
